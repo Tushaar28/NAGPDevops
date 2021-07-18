7e29eb2852f2fd15a3e4f322590f77882b369a7d
Tushaar Tiwari <35057803+Tushaar28@users.noreply.github.com>
2021-07-18 20:36:39 +0530

    Update AppTest.java

--- src/test/java/com/tushaar/nagp/devops/AppTest.java
+++ src/test/java/com/tushaar/nagp/devops/AppTest.java
@@ -13,6 +13,12 @@ public class AppTest extends TestCase
 	public int sub(int a, int b) {
 		return a - b;
 	}
+	
+	public String getMessage(String message) {
+		if(message.length() == 0)
+			return "Empty message";
+		return message;
+	}
 
 	@Test
 	public void testAdd() {
@@ -33,5 +39,23 @@ public class AppTest extends TestCase
 		int total = 0;
 		int sub = sub(4, 4);
 		assertEquals(sub, total);
+	}
+	
+	@Test
+	public void testMessage() {
+		String result = getMessage("");
+		assertEquals(result, "Empty message");
+	}
+	
+	@Test
+	public void testMessage1() {
+		String result = getMessage("Hello");
+		assertEquals(result, "Hello");
+	}
+	
+	@Test
+	public void testMessage2() {
+		String result = getMessage("Hello");
+		assertNotEquals(result, "Empty message");
 	}
 }
